4f7fedf9f75689ebb1a25b26f9c46e266ff567a6
andrenicsx <@andrenicolasgc@gmail.com>
2026-03-22 23:26:38 -0300

    Finalizado: Endpoints Cliente, Funcionario e Produto com Testes

---
+++ src/app/ProdutoRouter.py
@@ -0,0 +1,22 @@
+# André Nícolas Granemann Coelho
+
+from fastapi import APIRouter
+from domain.entities.Produto import Produto
+
+router = APIRouter()
+
+@router.get("/produto/", tags=["Produto"], status_code=200)
+def get_produto():
+    return {"msg": "produto get todos executado"}
+
+@router.post("/produto/", tags=["Produto"], status_code=200)
+def post_produto(corpo: Produto):
+    return {"msg": "produto post executado", "nome": corpo.nome, "valor": corpo.valor_unitario}
+
+@router.put("/produto/{id}", tags=["Produto"], status_code=200)
+def put_produto(id: int, corpo: Produto):
+  return {"msg": "produto put executado", "id": id, "nome": corpo.nome, "cpf": corpo.cpf, "telefone": corpo.telefone}
+
+@router.delete("/produto/{id}", tags=["Produto"], status_code=200)
+def delete_produto(id: int):
+  return {"msg": "produto delete executado", "id":id}
---
+++ src/domain/entities/Cliente.py
@@ -0,0 +1,10 @@
+# André Nícolas Granemann Coelho
+from pydantic import BaseModel
+
+
+class Cliente(BaseModel):
+  id_cliente: int = None
+  nome: str
+  cpf: str
+  telefone: str
+  
