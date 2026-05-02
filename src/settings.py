fa7fe2861a39488a41dcc0c7c0bd0a150baf6255
andrenicsx <@andrenicolasgc@gmail.com>
2026-05-02 20:29:57 -0300

    comandas

--- src/settings.py
+++ src/settings.py
@@ -10,9 +10,9 @@ load_dotenv(dotenv_file)
 BASE_DIR = Path(__file__).resolve().parent
 
 # Configurações da API
-HOST = os.getenv("HOST")
-PORT = os.getenv("PORT")
-RELOAD = os.getenv("RELOAD")
+HOST = os.getenv("HOST", "0.0.0.0")
+PORT = os.getenv("PORT", "8000")
+RELOAD = os.getenv("RELOAD", True)
 
 # Configurações banco de dados
 DB_SGDB = os.getenv("DB_SGDB")
@@ -25,11 +25,15 @@ DB_PASS = os.getenv("DB_PASS")
 
 # Ajusta STR_DATABASE conforme gerenciador escolhido
 if DB_SGDB == "sqlite":
-    # Cria um caminho absoluto para o arquivo .db
     DB_PATH = BASE_DIR / f"{DB_NAME}.db"
     STR_DATABASE = f"sqlite:///{DB_PATH}"
-    print(f"--- BANCO DE DADOS EM: {DB_PATH} ---")
-    print(f"--- DATABASE LOCAL: {STR_DATABASE} ---")
+
+# Configurações de database assíncrono
+# Converte string de conexão para async se necessário
+if STR_DATABASE.startswith("sqlite:///"):
+    ASYNC_STR_DATABASE = STR_DATABASE.replace("sqlite:///", "sqlite+aiosqlite:///")
+elif STR_DATABASE.startswith("sqlite://"):
+    ASYNC_STR_DATABASE = STR_DATABASE.replace("sqlite://", "sqlite+aiosqlite:///")
 
 # Configurações JWT
 SECRET_KEY = os.getenv("SECRET_KEY", "d6cf05df57c5fe76a488f84827a35851c0b718e465b478937fc97a95a0fd7a01")
